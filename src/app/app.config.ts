677b0c1c8fb405737427ebabfeec84adc2aecf75
Kyryll <glum.kiril@gmail.com>
2023-09-15 13:43:36 +0300

    Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      HttpClientModule
+    )
+  ]
 };
